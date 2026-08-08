006991668f834d3fbe160ec6f86250dd97a8a84a
Subham Pathak <subhamhimself@gmail.com>
2026-08-08 13:08:13 +0530

    WIP

--- server/serve.ps1
+++ server/serve.ps1
@@ -11,9 +11,11 @@ if (-not $LlamaServer) {
 $Args = @(
     "--host", "0.0.0.0",
     "--port", "8080",
+    "--models-max", "1",
     "--models-preset", "$PSScriptRoot\models.ini",
     "--log-file", "$PSScriptRoot\logs.txt",
     "--no-mmap"
+    "--no-mmproj"
 )
 
 & $LlamaServer @Args
